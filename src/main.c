51de615e369c19d954e3390327312eec9162b210
zenosaaur <davidezeni02@gmail.com>
2023-05-02 18:54:29 +0200

    implementazione menu data e ora

--- src/main.c
+++ src/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <termios.h>
-
+#include <time.h>
 
 #define superVisorCode "2244"
 // funzione per pulire il terminale
 #define clrscr() printf("\e[1;1H\e[2J")
 
-//TODO: Approfondire argomento
-int getch() {
+// TODO: Approfondire argomento
+int getch()
+{
     int ch;
     // struct to hold the terminal settings
     struct termios old_settings, new_settings;
     // take default setting in old_settings
     tcgetattr(0, &old_settings);
-    // make of copy of it (Read my previous blog to know 
+    // make of copy of it (Read my previous blog to know
     // more about how to copy struct)
     new_settings = old_settings;
     // change the settings for by disabling ECHO mode
@@ -38,7 +39,10 @@ int main(int argc, char *argv[])
     // Counter del menu
     int menuCounter = 0;
     // array delle voci del menu
-    char* menuString[6] = {"Setting automobile:", "Data:", "Ora:","Blocco automatico porte:","Back-home:","Check olio"};
+    char *menuString[6] = {"Setting automobile:", "Data:", "Ora:", "Blocco automatico porte:", "Back-home:", "Check olio"};
+    // Genero struct contenente la data di oggi
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
     // TODO: funzione in Assembly che preso in input due stringhe e le compara
     if (userParameter != NULL)
     {
@@ -58,19 +62,17 @@ int main(int argc, char *argv[])
     while (1)
     {
         if (getch() == '\033')
-        {   
+        {
             getch();
             switch (getch())
             {
             case 'A':
                 clrscr();
                 menuCounter > 0 ? menuCounter-- : menuCounter;
-                printf("%s\n",menuString[menuCounter]); 
                 break;
             case 'B':
                 clrscr();
-                menuCounter < 5  ? menuCounter++ : menuCounter;
-                printf("%s\n",menuString[menuCounter]); 
+                menuCounter < 5 ? menuCounter++ : menuCounter;
                 break;
             case 'C':
                 printf("freccia in destra\n");
@@ -79,11 +81,42 @@ int main(int argc, char *argv[])
                 printf("freccia in sinistra\n");
                 break;
             }
-        }else if (getch() == 'q')
+            switch (menuCounter)
+            {
+            case 0:
+                printf("%d: %s ", menuCounter, menuString[menuCounter]);
+                if (isSuperVisor)
+                {
+                    printf("(supervisor)\n");
+                }
+                else
+                {
+                    printf("\n");
+                }
+
+                break;
+            case 1:
+                printf("%d: %s ", menuCounter, menuString[menuCounter]);
+                printf("%02d-%02d-%02d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+                break;
+            case 2:
+                printf("%d: %s ", menuCounter, menuString[menuCounter]);
+                printf("%02d:%02d:%02d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+                break;
+                printf("%d: %s\n", menuCounter, menuString[menuCounter]);
+                break;
+            case 4:
+                printf("%d: %s\n", menuCounter, menuString[menuCounter]);
+                break;
+            case 5:
+                printf("%d: %s\n", menuCounter, menuString[menuCounter]);
+                break;
+            }
+        }
+        else if (getch() == 'q')
         {
             break;
         }
-        
     }
 
     return 0;
