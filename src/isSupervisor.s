70fa22a4cd7eb44ab1a90e874c03af029dd17261
zenosaaur <davidezeni02@gmail.com>
2023-05-09 00:33:53 +0200

    stampa menu admin o no

--- src/isSupervisor.s
+++ src/isSupervisor.s
@@ -2,26 +2,88 @@
 .section .data
     supervisorCode: .string  "2244"
     supervisor: .string  "menu supervisor"
-    test2: .string  "menu normale"
+    lenSupervisor: .long . - supervisor
+    user: .string  "menu normale"
+    lenUser: .long . - user
+    new_line_char:
+	.byte 10
 
 .section .text
     .global _start
 
 _start:
-    movl 8(%esp), %eax
-    pushl %ebp
-    movl %esp, %ebp /*salvo in ebp il puntatore a esp*/
-    movl 8(%ebp), %ebx /*argomento passato dalla funzione main 2244 o null*/
-    /*comparazione*/
-    cmp supervisorCode, %ebx 
-    je isSupervisor
-    jmp notSupervisor
-        /*menu*/
-isSupervisor:
-    /*menuSupervisor*/
-notSupervisor:
-    /*menu*/
+    popl %ecx /*Scarto i primi due elementi 
+                dello Stack che contiene il numero 
+                di parametri e il comando ./eseguibile
+                */
+    popl %ecx
+adminMenager:
+    popl %ecx  /*Dominio: 2244(admin user), null*/
+    testl %ecx, %ecx
+    jz userRedirect
+    cmp %ecx, %ecx
+    je supervisorRedirect
+supervisorRedirect:
+    movl $4, %eax
+	movl $1, %ebx
+	leal supervisor, %ecx
+	movl lenSupervisor, %edx
+	int $0x80
+
+    movl $4, %eax
+	movl $1, %ebx
+	leal new_line_char, %ecx
+	movl $1, %edx
+	int $0x80
+    jmp fine
+userRedirect:
+    movl $4, %eax
+	movl $1, %ebx
+	leal user, %ecx
+	movl lenUser, %edx
+	int $0x80
+
+    movl $4, %eax
+	movl $1, %ebx
+	leal new_line_char, %ecx
+	movl $1, %edx
+	int $0x80
+    jmp fine
 fine:
-    movl $1, %eax
+	movl $1, %eax
+	movl $0, %ebx
+	int $0x80
+
+# --------FUNZIONI---------
+.type printArgs, @function 	#  Stampa stringa
+printArgs:
+    # Cose  necessarie: il numero di caratteri
+    call countChar
+    movl $4, %eax
+    movl $1, %ebx
+    
     int $0x80
 
+    # stamp o 
+    movl $4, %eax
+	movl $1, %ebx
+	leal new_line_char, %ecx
+	movl $1, %edx
+	int $0x80
+	
+
+# --------------------------------------
+.type countChar, @function			
+
+countChar:
+	xorl %edx, %edx # il contenuto del risultato viene salavato in %edx
+
+iterate:
+	movb (%ecx,%edx), %al 			# mette il carattere della stringa in al
+	testb %al, %al 					# se il carattere è 0 (\0) la stringa è finita
+	jz end_count				
+	incl %edx
+	jmp iterate
+
+end_count:
+	ret
